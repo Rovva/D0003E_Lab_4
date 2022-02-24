ea1de1ec2eb5fb8d509d86206bb01c564f1d03ac
Christoffer Rova <rovva@hostarn.se>
2022-02-24 21:30:21 +0100

    Only need to comment and fix joystickinterrupt

--- Joystick.h
+++ Joystick.h
@@ -7,15 +7,21 @@
 
 #ifndef JOYSTICK_H_
 #define JOYSTICK_H_
+#include <avr/io.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
 
+#include "TinyTimber.h"
 #include "GUI.h"
 
 typedef struct {
 	Object super;
 	GUI *gui;
+	bool firstClick;
 } Joystick;
 
 void joystickInterrupt(Joystick *self);
 
-#define initJoystick(gui) { initObject(), gui }
+#define initJoystick(gui) { initObject(), gui, true }
 #endif /* JOYSTICK_H_ */
